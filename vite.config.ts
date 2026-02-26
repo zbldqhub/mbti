0e30500b887069efd1dba0acbd6446d82073acbe
zbldqhub <61268618+zbldqhub@users.noreply.github.com>
2026-02-27 01:08:37 +0800

    Add files via upload
    修改路由4

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/mbti/' : './',
+  base: './',
   plugins: [vue()],
   resolve: {
     alias: {
